21d69a82d68bca4f762dc9483ab903b154cae9e7
tamiaquiyara <mia.mbuckingworth@gmail.com>
2022-09-07 14:18:55 +0200

    created price plan screen

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 let express = require('express');
-let app = express();
+// const flash = require('express-flash');
+// const session = require('express-session');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+// const Greeting = require('./greet');
 
-app.get("/", function(req, res){
-  res.send("Total PhoneBill WebApp");
+
+const app = express();
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+
+app.post("/calc_bill", function (req, res) {
+    res.redirect("/");
+});
+
+
+app.get("/price_plan", function (req, res) {
+    res.redirect("/");
+});
+
+
+app.get("/price_plan/:id", function (req, res) {
+    res.redirect("/");
+});
+
+
+
+app.get("/user", function (req, res) {
+    res.redirect("/");
+});
+
+
+
+app.post("/link_user", function (req, res) {
+    res.redirect("/");
 });
 
 let PORT = process.env.PORT || 3004;
 
-app.listen(PORT, function(){
-  console.log('App starting on port', PORT);
+app.listen(PORT, function () {
+    console.log('App starting on port', PORT);
 });
